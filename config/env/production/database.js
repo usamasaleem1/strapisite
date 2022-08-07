cc67f53697ed2da9cec3370f78fa3119bf0dcd4c
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2022-08-07 19:14:56 -0400

    updated db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,19 @@
-// path: ./config/database.js
+// path: ./config/env/production/database.js
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'),
-            schema: env('DATABASE_SCHEMA', 'public'), // Not required
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            user: config.user,
+            password: config.password,
             ssl: {
-                ca: env('DATABASE_CA')
+                rejectUnauthorized: false
             },
         },
         debug: false,
