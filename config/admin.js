7dd6371b759b30cfd4170c6c3f96b2b00645cbe4
BJR-Developer <freelancerbjr@gmail.com>
2022-08-11 22:01:12 +0600

    added user form collection

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '26254d2d4bea9e0986f97296e106fab4'),
+    secret: env('ADMIN_JWT_SECRET', 'vozpZrax8lppks1F42XTv1DWbdD4CSnrEGJd5wxaweQ='),
   },
 });
--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', "Tl13efB+DTq6jyT4h3jZ5Q==,Ei/4KdHbXeI0AR1IKV5E4A==,GuQhQCn14e4GBmfZntwfGQ==,aioXJO975/MFOqjZkT5iXw=="),
   },
 });
